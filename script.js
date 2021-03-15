24f0dbf3329de31cc119da18ba9c6a499cadc1d0
CorneliaRoehrer <corne.lia.roehrer@gmail.com>
2021-03-15 13:58:45 +0100

    added price total

--- script.js
+++ script.js
@@ -20,10 +20,69 @@ function addItem(e){
     // don't take "getElementsByClassName" here
     let itemTitle = item.querySelector(".card-title").innerText; 
     let itemPrice = item.querySelector('.card-price').innerText.replace("€", "");
+    let itemPicture = item.querySelector(".card-img-top").src;
+    rowCreate(itemTitle, itemPrice, itemPicture);
     
-    
-    console.log(itemTitle, itemPrice);
-
-
-
+    console.log(itemTitle, itemPrice, itemPicture);
 }
+
+function rowCreate(itemTitle, itemPrice, itemPicture){
+    // create variables for use of for-loop that +1 in the shopping cart
+    let cartItems = document.getElementById('cart-items');
+    let cartItemsNames = cartItems.getElementsByClassName('cart-item-title');
+    let cartItemQtt = cartItems.getElementsByClassName('cart-quantity');
+
+    // this loop adds +1 to an item already in the cart instead of adding the item again
+    for (let i = 0; i < cartItemsNames.length; i++) {
+        if (cartItemsNames[i].innerText == itemTitle) {
+            alert("This item already exists in your cart");
+            let qtt = Number(cartItemQtt[i].innerHTML);
+            cartItemQtt[i].innerHTML = qtt + 1 ;
+           // console.log(qtt);
+            updateTotal();        
+            return ;//it will stop our script
+        }
+}
+
+    let item = `
+    <div class="cart-row row d-flex ">
+        <div class="cart-item col-6 my-3 ">
+            <img class="cart-item-image" src="${itemPicture}" width="100" height="100">
+            <span class="cart-item-title h5 ">${itemTitle}</span>
+        </div>
+
+        <span class="cart-price col-3 h4 my-3">€ ${itemPrice}</span>
+
+        <div class="cart-qtty-action col-3 d-flex">           
+            <i class="minus fa fa-minus-circle my-auto" ></i>            
+            <div class="cart-quantity p-4 h4">1</div>            
+            <i class="plus fa fa-plus-circle my-auto"></i>        
+            <button class="del btn btn-danger rounded-circle  my-auto ms-3 fw-bold" type="button"> X </button>            
+        </div>
+    </div>`;
+    let cart = document.getElementById('cart-items');
+    cart.innerHTML += item;
+    // documentReady();
+    console.log(item);
+}
+
+// adding the prices of the items in the shopping cart and updating the total price
+function updateTotal() {
+    let cart = document.getElementById("cart-items");
+    let cartRows = cart.getElementsByClassName("cart-row");
+    let total = 0; // it will be calculated from zero each time it is updated
+    for (let i = 0; i < cartRows.length; i++) {
+        let cartRow = cartRows[i];
+        let price = parseFloat(cartRow.getElementsByClassName( "cart-price")[0].innerText.replace("€", ""));
+
+ //we only need the first one
+        let qtt = Number(cartRow.getElementsByClassName("cart-quantity")[0].innerText);
+        console.log(price, qtt);
+        total += (price * qtt);
+        console.log(total);
+    }
+    total = total.toFixed(2);//toFixed() will help rounding the number to 2 decimals
+    let totalElement = document.getElementById("total-price").querySelector( '#total-price-sum');
+    // console.log(total);
+    totalElement.innerHTML = "€" + total;
+};
